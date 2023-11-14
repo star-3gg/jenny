5b6b6ab70f3f4dd090b714dbf68530cd991998d7
star-3gg <147496446+star-3gg@users.noreply.github.com>
2023-11-14 11:49:19 +0000

    adjustable project build script for container and local build

---
+++ run_checks_and_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cppcheck --enable=warning,performance,portability --error-exitcode=1 --language=c++ --std=c++17 /app/src /app/include && \
+# Disable AdressSanitizer and ThreadSanitizer
+cmake -S . -B build -DENABLE_ASAN=OFF -DENABLE_TSAN=OFF && \
+make -C build
