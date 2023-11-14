28d58d9fa25eddf511b7928688c7702f437b7d70
star-3gg <147496446+star-3gg@users.noreply.github.com>
2023-11-14 18:50:07 +0000

    working plot demo ui

--- run_checks_and_build.sh
+++ run_checks_and_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Run cppcheck
 cppcheck --enable=warning,performance,portability --error-exitcode=1 --language=c++ --std=c++17 /app/src /app/include && \
 # Disable AdressSanitizer and ThreadSanitizer
 cmake -S . -B build -DENABLE_ASAN=OFF -DENABLE_TSAN=OFF && \
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,85 @@
+// // /**
+// //  * @brief Prints a greeting message to the console.
+// //  *
+// //  * This function demonstrates basic output to the console using std::cout. It can be
+// //  * expanded to include more complex functionality or more detailed messages.
+// //  * 
+// //  * @param message The greeting message to be printed.
+// //  */
+// // void printGreeting(const std::string& message) {
+// //     std::cout << message << std::endl;
+// // }
+
+// // /**
+// //  * @brief Main entry point of the program.
+// //  * 
+// //  * This main function calls printGreeting to demonstrate basic output functionality.
+// //  * 
+// //  * @return int Returns 0 to indicate successful execution.
+// //  */
+// // int main() {
+// //     printGreeting("Hello Security World!");
+// //     return 0;
+// // }
+// #include <QApplication>
+// #include <QMainWindow>
+// #include "adapters/gui/qt-ui-components/ui_textinterface.h"  // Include the generated header file
+
 // /**
-//  * @brief Prints a greeting message to the console.
+//  * @brief Main program
+//  * 
+//  * Runs a simple UI component for Qt testing purposes
 //  *
-//  * This function demonstrates basic output to the console using std::cout. It can be
-//  * expanded to include more complex functionality or more detailed messages.
-//  * 
-//  * @param message The greeting message to be printed.
+//  * @param argc 
+//  * @param argv 
+//  * @return int 
 //  */
-// void printGreeting(const std::string& message) {
-//     std::cout << message << std::endl;
+// int main(int argc, char *argv[]) {
+//     // QCustomPlot plot test
+//     // TODO or stick with qcharts
+
+//     // QT ui component test
+//     QApplication app(argc, argv);
+//     QMainWindow window;
+
+//     Ui::Form ui;  // Use the generated UI class
+//     ui.setupUi(&window);
+
+//     window.show();
+//     return app.exec();
 // }
 
-// /**
-//  * @brief Main entry point of the program.
-//  * 
-//  * This main function calls printGreeting to demonstrate basic output functionality.
-//  * 
-//  * @return int Returns 0 to indicate successful execution.
-//  */
-// int main() {
-//     printGreeting("Hello Security World!");
-//     return 0;
-// }
-#include <QApplication>
-#include <QMainWindow>
-#include "adapters/gui/qt-ui-components/ui_textinterface.h"  // Include the generated header file
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
 
-/**
- * @brief Main program
- * 
- * Runs a simple UI component for Qt testing purposes
- *
- * @param argc 
- * @param argv 
- * @return int 
- */
+QT_CHARTS_USE_NAMESPACE
+
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    QApplication a(argc, argv);
+
+    // Create a line series
+    QLineSeries *series = new QLineSeries();
+    series->append(0, 6);
+    series->append(2, 4);
+    // ... add more points as needed
+
+    // Create a chart
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple Line Chart Example");
+
+    // Display the chart
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
     QMainWindow window;
-
-    Ui::Form ui;  // Use the generated UI class
-    ui.setupUi(&window);
-
+    window.setCentralWidget(chartView);
+    window.resize(400, 300);
     window.show();
-    return app.exec();
+
+    return a.exec();
 }
