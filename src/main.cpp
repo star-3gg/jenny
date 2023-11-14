79bcc1d53036e85aa011023d68e7ee133620c2c3
star-3gg <147496446+star-3gg@users.noreply.github.com>
2023-11-14 13:47:49 +0000

    doxygen test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
-#include <cstring>
-#include <iostream>
+/**
+ * @brief Prints a greeting message to the console.
+ *
+ * This function demonstrates basic output to the console using std::cout. It can be
+ * expanded to include more complex functionality or more detailed messages.
+ * 
+ * @param message The greeting message to be printed.
+ */
+void printGreeting(const std::string& message) {
+    std::cout << message << std::endl;
+}
 
 /**
- * @brief Main Program
+ * @brief Main entry point of the program.
  * 
- * @return int 
+ * This main function calls printGreeting to demonstrate basic output functionality.
+ * 
+ * @return int Returns 0 to indicate successful execution.
  */
 int main() {
-    std::cout << "Hello Security World!";
+    printGreeting("Hello Security World!");
     return 0;
 }
