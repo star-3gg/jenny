2278b644d19ac6286dbc7ab9771978310bd21fdb
star-3gg <147496446+star-3gg@users.noreply.github.com>
2023-11-14 18:07:45 +0000

    Working demo UI and quieter doxygen service

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,20 +2,20 @@
 # Generate config if not present and generate documentation
 cd /doxy/docs
 if [ ! -f "Doxyfile" ]; then doxygen -g; fi
-doxygen Doxyfile
+doxygen Doxyfile 1> /dev/null
 
 # Generate pdf from tex
-cd /doxy/docs/latex
+cd /doxy/docs/latex 1> /dev/null
 
 # Run pdflatex to compile the TeX files into a PDF
-pdflatex refman.tex
+pdflatex refman.tex 1> /dev/null
 
 # Run makeindex if it's needed (this step generates the index file)
-makeindex refman.idx
+makeindex refman.idx 1> /dev/null
 
 # Run pdflatex again to generate the PDF (run it multiple times to resolve references)
-pdflatex refman.tex
-pdflatex refman.tex
+pdflatex refman.tex 1> /dev/null
+pdflatex refman.tex 1> /dev/null
 
 # If a documentation has been generated, copy it to the main directory
-if [ -f "refman.pdf" ]; then cp refman.pdf /doxy/docs/documentation.pdf; fi
+if [ -f "refman.pdf" ]; then cp refman.pdf /doxy/docs/documentation.pdf 1> /dev/null; fi
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,46 @@
-/**
- * @brief Prints a greeting message to the console.
- *
- * This function demonstrates basic output to the console using std::cout. It can be
- * expanded to include more complex functionality or more detailed messages.
- * 
- * @param message The greeting message to be printed.
- */
-void printGreeting(const std::string& message) {
-    std::cout << message << std::endl;
-}
+// /**
+//  * @brief Prints a greeting message to the console.
+//  *
+//  * This function demonstrates basic output to the console using std::cout. It can be
+//  * expanded to include more complex functionality or more detailed messages.
+//  * 
+//  * @param message The greeting message to be printed.
+//  */
+// void printGreeting(const std::string& message) {
+//     std::cout << message << std::endl;
+// }
+
+// /**
+//  * @brief Main entry point of the program.
+//  * 
+//  * This main function calls printGreeting to demonstrate basic output functionality.
+//  * 
+//  * @return int Returns 0 to indicate successful execution.
+//  */
+// int main() {
+//     printGreeting("Hello Security World!");
+//     return 0;
+// }
+#include <QApplication>
+#include <QMainWindow>
+#include "adapters/gui/qt-ui-components/ui_textinterface.h"  // Include the generated header file
 
 /**
- * @brief Main entry point of the program.
+ * @brief Main program
  * 
- * This main function calls printGreeting to demonstrate basic output functionality.
- * 
- * @return int Returns 0 to indicate successful execution.
+ * Runs a simple UI component for Qt testing purposes
+ *
+ * @param argc 
+ * @param argv 
+ * @return int 
  */
-int main() {
-    printGreeting("Hello Security World!");
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow window;
+
+    Ui::Form ui;  // Use the generated UI class
+    ui.setupUi(&window);
+
+    window.show();
+    return app.exec();
 }
